3e87914399f911869639d74b1626df1077d614e5
sammy0101 <59214670+sammy0101@users.noreply.github.com>
2026-02-01 10:31:55 +0000

    🔄 自動同步 _worker.js (📅 2026-02-01 18:31)

--- _worker.js
+++ _worker.js
@@ -1,5 +1,5 @@
 ﻿import //crypto from "node:crypto";
-{ connect }
+/*How can we utilize the*/ { connect }
 /*function*/ from /*the*/ "cloudflare:sockets";//​ library in this Worker?
 let config_JSON, 反代IP = '', 启用SOCKS5反代 = null, 启用SOCKS5全局反代 = false, 我的SOCKS5账号 = '', parsedSocks5Address = {};
 let 缓存反代IP, 缓存反代解析数组, 缓存反代数组索引 = 0, 启用反代兜底 = true;
@@ -2114,4 +2114,3 @@ async function html1101(host, 访问IP) {
 </body>
 </html>`;
 }
-
